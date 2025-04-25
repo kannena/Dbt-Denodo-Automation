f22f8ce05577601f5c96389c13516035c08387b0
kannena <138162691+kannena@users.noreply.github.com>
2025-04-24 22:00:56 -0500

    Update generate_denodo_base.py

--- scripts/generate_denodo_base.py
+++ scripts/generate_denodo_base.py
@@ -38,7 +38,7 @@ denodo_type_map = {
 
 # Start writing VQL
 vql_lines = [
-    '# REQUIRES-PROPERTIES-FILE - # Do not remove this comment!',
+
     f'CREATE OR REPLACE WRAPPER JDBC "{table_name}"',
     '    FOLDER = \'/1. virtual model/1. connectivity/2. base views/1. source views\'',
     '    DATASOURCENAME=rsiconnections."ds_CLOUD_DW"',
